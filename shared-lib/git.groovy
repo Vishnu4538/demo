c99662e619fdb651c4ecb962102dc9059d8b15ed
vishnu <edlavishnu@aol.com>
2025-12-07 19:59:05 +0530

    modifiied the existing

--- shared-lib/git.groovy
+++ shared-lib/git.groovy
@@ -1,4 +1,4 @@
-def unit_test(string "git_user"){
+def call(string "git_user"){
 
                   echo "pulling the github repo"
                   git url: 'https://github.com/${git_user}/devsecops-demo.git', branch: 'main'
