ffd993080960327d6373eb96ca1f28fb42861452
Vishnu4538 <edlavishnu@aol.com>
2025-12-07 20:23:24 +0530

    Update git.groovy

--- shared-lib/vars/git.groovy
+++ shared-lib/vars/git.groovy
@@ -1,7 +1,7 @@
 def call(string git_user){
 
                   echo "pulling the github repo"
-                  git url: 'https://github.com/${git_user}/devsecops-demo.git', branch: 'main'
+                  git url: 'https://github.com/"${git_user}"/devsecops-demo.git', branch: 'main'
                   echo "Unit testing"
                   sh 'npm install'
                   sh 'npm audit fix --force || true'
