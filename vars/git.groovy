403af95ba00912619716be32d3cccfbc74f9b5e8
Vishnu4538 <edlavishnu@aol.com>
2025-12-07 20:53:57 +0530

    Update git.groovy

--- vars/git.groovy
+++ vars/git.groovy
@@ -2,8 +2,11 @@ def call(String git_user){
 
                   echo "pulling the github repo"
                   git url: "https://github.com/${git_user}/devsecops-demo.git", branch: 'main'
-                  echo "Unit testing"
-                  sh 'npm install'
-                  sh 'npm audit fix --force || true'
-                  sh 'npm run test'
+                 echo "Unit testing"
+    def status = sh(script: 'npm install && npm run test', returnStatus: true)
+    if (status != 0) {
+        echo "Unit test failed with exit code ${status}"
+        // optionally: error("Stopping pipeline due to test failure")
+    }
 }
+
